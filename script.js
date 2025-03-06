3e3e024ea4f3c7e1fc1710fb8d0ee7fe108a6f41
Bronks8365 <116671291+Bronks8365@users.noreply.github.com>
2025-03-06 19:08:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-function detalhesLivro(url) {
-    window.location.href = url;
+function comprarLivro() {
+    window.location.href = "https://pay.kirvano.com/f4f45ced-de52-4f71-b1e2-a8079e5ef417";
 }
